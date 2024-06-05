7c51d5b3d33eb56b725806ed21988b6fa7d95468
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-06-05 19:16:13 +0530

    Complete UI Of Login

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,11 @@
 
 import  { Route, Routes } from 'react-router-dom';
-import Signup from './Component/SignUp/SignUp';
+import Auth from './Component/Auth/Auth';
 const App = () => (
   <div className='App'>
   <Routes>
-<Route path='/signup' element={<Signup/>}></Route>
+<Route path='/signup' element={<Auth Account="SignUp"/>}></Route>
+<Route path='/signin' element={<Auth Account="SignIn"/>}></Route>
   </Routes>
   </div>
 );

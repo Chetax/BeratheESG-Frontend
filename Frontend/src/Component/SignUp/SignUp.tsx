f8e6270a429d0d1e4f8fccd7f8ae49f6e54d193e
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-06-05 18:24:41 +0530

    Complete SignUp Page

--- Frontend/src/Component/SignUp/SignUp.tsx
+++ Frontend/src/Component/SignUp/SignUp.tsx
@@ -1,8 +1,10 @@
 import './SignUp.scss'
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from './Images/Logo.png'
 import earth from './Images/Earth.png'
-import { Col,Image, Row, Layout ,Typography, Flex} from 'antd';
+import {Form ,Button,Input} from 'antd'
+import { Col,Image, Row, Layout ,Typography} from 'antd';
 
 const { Title } = Typography;
 const SignUp:React.FC=()=> {
@@ -13,7 +15,7 @@ const SignUp:React.FC=()=> {
   <Row>
   <Col span={12} style={{marginTop:'35vh'}}>
     <Row>
-        <Col span={10}  ></Col>
+        <Col span={10}></Col>
         <Col span={10}  >
 
     <div style={{}}> 
@@ -25,15 +27,29 @@ const SignUp:React.FC=()=> {
     </Col>
     </Row>
   </Col>
-
-      <Col span={12}>
-     <div>
-      <Image src={earth}></Image>
-      <form action="">
+      <Col span={10} style={{marginTop:'4vh'}}>
+      
+      <Image src={earth} style={{width:'20%',marginLeft:'20%'}}></Image>
+      
+       <Form wrapperCol={{span:14}} style={{padding:'5%',position:'relative',backgroundColor:"rgba(35, 94, 74, 1)",width:"60% ",height:"88%"}} >
+   <Title level={4} style={{color:'white' }}>Sign Up</Title>
+      
+    
+        <Form.Item name='email' label={<span style={{ color: 'white' }}>Email</span>} rules={[{ required:true, message:'Please Enter Email'    }]} > </Form.Item>
+        <Input placeholder='Enter Your Email'style={{height:"6vh"}} ></Input>
         
-      </form>
-     </div>
 
+        <Form.Item name='password' label={<span style={{ color: 'white',marginTop:'20%' }}>Password</span>}>   </Form.Item>
+        <Input.Password placeholder='Enter Your password' style={{height:"6vh"}}></Input.Password>
+      
+
+        <Form.Item name='Conformpassword' label={<span style={{ color: 'white',marginTop:'20%' }}>Conform Password</span>}>   </Form.Item>
+        <Input.Password placeholder='Conform Your Password ' style={{height:"6vh"}}></Input.Password>
+      
+        <Button type='primary' style={{position:'absolute', backgroundColor:"rgba(46, 152, 68, 1)",width:'50%',left:'25%',marginTop:"60px",}}>Continue</Button>
+             
+        <Title level={5} style={{position:'absolute',width:'50%',bottom:0,color:'white'}}>Already Member ? <NavLink to='/signin' ><span style={{cursor:'pointer',marginLeft:'10px', color:'rgba(79, 165, 86, 1)',fontWeight:'bolder'}}>   Login </span></NavLink></Title>
+       </Form>
 
       </Col>
   

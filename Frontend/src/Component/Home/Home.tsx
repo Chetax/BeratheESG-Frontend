edaf6ca69d93e5e1674c80af81def7621f603abb
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-06-06 13:42:20 +0530

    Done With Both Dashbaord

--- Frontend/src/Component/Home/Home.tsx
+++ Frontend/src/Component/Home/Home.tsx
@@ -4,14 +4,16 @@ import { CiBellOn,CiShare2 } from "react-icons/ci";
 import { MdOutlinePieChartOutline,MdDeleteOutline } from "react-icons/md";
 import { FaRegUserCircle ,FaBuilding} from "react-icons/fa";
 import { SiGoogledocs,SiPivotaltracker } from "react-icons/si";
+import { LuMessageSquarePlus } from "react-icons/lu";
+import { LuGoal } from "react-icons/lu";
 import { GoStack } from "react-icons/go";
 import { RiImageCircleLine } from "react-icons/ri";
 import { TbTargetArrow } from "react-icons/tb";
 import { IoAnalyticsSharp } from "react-icons/io5";
 import { FiLogOut } from "react-icons/fi";
 import Logo from "../Auth/Images/Logo.png";
-import {  MenuFoldOutlined,  MenuUnfoldOutlined,  BarChartOutlined,} from "@ant-design/icons";
-import { Button, Layout, Menu, theme, Select, Space, Table, Tag } from "antd";
+import {  MenuFoldOutlined,  MenuUnfoldOutlined,  BarChartOutlined,SearchOutlined} from "@ant-design/icons";
+import { Button, Layout, Card,Menu, theme, Select, Space, Table, Tag,Input } from "antd";
 import type { TableProps } from 'antd';
 interface DataType {
   key: string;
@@ -24,8 +26,15 @@ interface DataType {
   result: string | undefined;
   action: undefined;
 }
+interface TrackerData{
+  month:string;
+  status: string;
+  complition:string;
+  business:string;
+}
 const { Header, Sider, Content } = Layout;
-
+const { Meta } = Card;
+const { Search } = Input;
 const Home: React.FC = () => {
   const [collapsed, setCollapsed] = useState<boolean>(false);
   const [dataentry,setdataentry]=useState<boolean>(true);
@@ -33,6 +42,7 @@ const Home: React.FC = () => {
 
   const columns: TableProps<DataType>['columns'] = [
     {
+
       title: 'Checkbox',
       dataIndex: 'checkbox',
       key: 'checkbox',
@@ -92,6 +102,32 @@ const Home: React.FC = () => {
       ),
     },
   ];
+  const column2:TableProps<TrackerData>['columns']=[
+    {
+      title: 'MONTH',
+      dataIndex: 'month',
+      key: 'month',
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: 'COMPLETION %',
+      dataIndex: 'complition',
+      key: 'complition',
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: 'business unit ',
+      dataIndex: 'business',
+      key: 'business',
+      render: (text) => <p>{text}</p>,
+    },
+  ]
   const data: DataType[] = [
     {
       key: '1',
@@ -149,7 +185,26 @@ const Home: React.FC = () => {
       action:undefined,
     }
   ];
-
+  const Trackerdata:TrackerData[]=[
+    {
+      month:"Jan 2023",
+      status:"PENDING APPROVAL (1/12)",
+      complition:"20%",
+      business:"Business Unit 1",
+    },
+    {
+      month:"Feb 2023",
+      status:"APPROVED (2/12)",
+      complition:"30%",
+      business:"Business Unit 1",
+    },
+    {
+      month:"Mar 2023",
+      status:"INCOMPLETE (4/12)",
+      complition:"50%",
+      business:"Business Unit 1",
+    },
+  ]
   return (
     <Layout style={{ height: "100vh", width: "100vw" }}>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -327,11 +382,29 @@ const Home: React.FC = () => {
 
           </div>
         </div>
-
-
-        <Table columns={columns} dataSource={data} />;
-
-
+        {dataentry===false?
+      <div style={{display:'flex'}}>
+        <div className="PrndingTracker">
+        <Card    hoverable    style={{ width: 240,margin:"10px" }} >
+    <div style={{display:'flex',justifyContent:"space-between",alignItems:'center'}}><h3 style={{color:"#9c9c9c"}} >PENDING TRACKERS</h3><LuGoal style={{fontSize:"25px",color:"#9c9c9c"}}/></div>
+    <h1>45/60</h1>
+    
+  </Card>
+        </div>
+        <div className="PrndingReviews">
+        <Card    hoverable    style={{ width: 240,margin:"10px" }} >
+    <div style={{display:'flex',justifyContent:"space-between",alignItems:'center'}}><h3 style={{color:"#9c9c9c"}} >PENDING REVIEWS</h3><LuMessageSquarePlus style={{fontSize:"25px",color:"#9c9c9c"}}/></div>
+    <h1>3</h1>
+    
+  </Card>
+        </div>
+      </div>
+:<>;</>}
+     <Space.Compact size="large">
+      <Input addonBefore={<SearchOutlined />} placeholder="Search for a business unit" style={{width:"25vw" ,margin:"10px 10px"}}/>
+      
+    </Space.Compact>    
+      {dataentry===true ?<Table columns={columns} dataSource={data} /> :<Table columns={column2} dataSource={Trackerdata} />}  
         </Content>
       </Layout>
     </Layout>
